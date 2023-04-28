914fdfdfa0adfb7f42fa98f7995151a3af6ccc7d
Dipansh Parmar <dipanshparmar870@gmail.com>
2023-04-28 12:13:53 +0530

    added preferred orientations

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 // pages
@@ -7,7 +8,14 @@ import 'pages/pages.dart';
 // providers
 import 'providers/providers.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // setting the preferred orientations
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+
   runApp(const MyApp());
 }
 
